a1549713af3d1d966e5c4143fd14f0745bdd144b
jacobspear7 <53849590+jacobspear7@users.noreply.github.com>
2019-08-07 08:01:15 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Data Bridge Management LLC, Designed by: Shenzhen Liku Technology Co. Ltd. </a></p>
+        <p className="copyright">&copy; Data Bridge Management LLC, Designed by: Shenzhen Liku Technology Co Ltd. </a></p>
     </footer>
 )
 
