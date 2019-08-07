c0979b4cecc7508c529ea4179a9da8befda3d466
jacobspear7 <53849590+jacobspear7@users.noreply.github.com>
2019-08-06 22:26:38 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
+    title: "Data Bridge Management",
     author: "Hunter Chang",
     description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
   },
