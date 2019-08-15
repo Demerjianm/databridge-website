9c0f647bd1fd56c0846c80b553ab798736150139
jacobspear7 <53849590+jacobspear7@users.noreply.github.com>
2019-08-14 17:53:34 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,9 +14,9 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Info</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Process</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About Us</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
