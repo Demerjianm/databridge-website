629391b2dc08ad6304f0d7c01ab7273b441eb77f
jacobspear7 <53849590+jacobspear7@users.noreply.github.com>
2019-09-20 09:13:42 +0400

    Create sendEmail.php

---
+++ src/components/sendEmail.php
@@ -0,0 +1,33 @@
+<?php
+if(isset($_POST['email'])) {
+ 
+
+    $email_to = "naqash263@gmail.com";
+    $email_subject = "Received Email from Customer";
+ 
+
+ 
+     
+ 
+    $name = $_POST['name']; // required
+    $email_from = $_POST['email']; // required
+    $message = $_POST['message']; // not required
+     
+ 
+    $email_message .= "First Name: ".clean_string($name)."\n"
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Message: ".clean_string($message)."\n";
+  
+ 
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+?>
+ 
+ 
+<?php
+ 
+}
+?>
